9ab8cccf91d8a94ad8ca3e2161aa901af698511a
Jacob <19701111@students.lincoln.ac.uk>
2022-09-01 15:22:39 +0100

    Added more JS functionality

--- tip-calculator-app-main/main.js
+++ tip-calculator-app-main/main.js
@@ -1,4 +1,5 @@
 const tipButtons = Array.from(document.getElementsByClassName("tip-button"));
+const resetButton = document.querySelector("#reset")
 const customInput = document.querySelector(".custom-tip");
 const displayTipPP = document.querySelector("#tip-pp");
 const displayTotalPP = document.querySelector("#total-pp");
@@ -7,11 +8,13 @@ const formPeople = document.querySelector("#people");
 
 let chosen_tip;
 
+
 function clearSelected() {
     tipButtons.forEach(element => {
         element.classList.remove("selected-button");
+        customInput.classList.remove("selected-button");
+        // Need to clear the value of customTip
         chosen_tip = null;
-    // Need to clear the custom form
     })
 }
 
@@ -28,24 +31,65 @@ function calcResults(bill, tip, num_people) {
     return {"tip_pp": tip_amount_pp, "total_pp": total_pp};
 }
 
+function validateForm(){
+    if ((chosen_tip != null) && (formBill.value > 0) && (formPeople.value > 0)){
+        console.log("TRUE")
+        return true
+    } else {
+        console.log("FALSE")
+        return false
+    }
+}
+
+// Need to figure out why this goes NaN
+function updateDisplay(){
+    if (validateForm()){
+        money_data = calcResults(formBill.value, chosen_tip, formPeople)
+        console.log(money_data['tip_pp'])
+        console.log(money_data['total_pp'])
+        displayTipPP.textContent = money_data['tip_pp']
+        displayTotalPP.textContent = money_data['total_pp']
+    }
+}
+
+function resetDisplay(){
+    displayTipPP.textContent = "0.00";
+    displayTotalPP.textContent = "0.00";
+}
+
+
 tipButtons.forEach(element => {
     element.addEventListener("click", () => {
         console.log(element.value);
         clearSelected();
         assignSelected(element);
         chosen_tip = parseFloat(element.value)
+        updateDisplay();
     })
 });
 
+// Need to reset the chosen_tip and selected class when value becomes empty
+// Also need to add % after text
 customInput.addEventListener("input", () => {
     clearSelected()
     customInput.classList.add("selected-button")
+    chosen_tip = parseFloat(customInput.value)
+    updateDisplay();
 })
 
+// Need to add regex to force format of ddd or ddd.dd
 formBill.addEventListener("input", () => {
-    
+    updateDisplay()
 })
 
 formPeople.addEventListener("input", () => {
-    
+    updateDisplay();
+})
+
+// Needs to clear the custom input field
+resetButton.addEventListener("click", () => {
+    formBill.value = "";
+    formPeople.value = "";
+    resetDisplay();
+    clearSelected();
 })

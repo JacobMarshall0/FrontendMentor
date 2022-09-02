dcb7154e96fd7993da3e976e8200d84e3c40db51
Jacob <19701111@students.lincoln.ac.uk>
2022-09-02 23:57:08 +0100

    Added error reset to error button, altered custom tip input, finalised solution

--- tip-calculator-app-main/main.js
+++ tip-calculator-app-main/main.js
@@ -1,16 +1,19 @@
 const tipButtons = Array.from(document.getElementsByClassName("tip-button"));
-const resetButton = document.querySelector("#reset")
+const resetButton = document.querySelector("#reset");
 const customInput = document.querySelector(".custom-tip");
 const displayTipPP = document.querySelector("#tip-pp");
 const displayTotalPP = document.querySelector("#total-pp");
 const formBill = document.querySelector("#bill");
-const formBillError = document.querySelector("#bill-error")
+const formBillError = document.querySelector("#bill-error");
 const formPeople = document.querySelector("#people");
-const formPeopleError = document.querySelector("#people-error")
+const formPeopleError = document.querySelector("#people-error");
+
+let errorTextArray = [formBillError, formPeopleError];
+let errorElementsArray = [formBill, formPeople, customInput];
 
 let chosen_tip;
 
-
+// Remove the selected class from the elements, and reset chosen tip value
 function clearSelected() {
     tipButtons.forEach(element => {
         element.classList.remove("selected-button");
@@ -19,10 +22,12 @@ function clearSelected() {
     })
 }
 
+// give an element the selected class
 function assignSelected(element) {
     element.classList.add("selected-button");
 }
 
+// Calculate the tip results and return it as an object
 function calcResults(bill, tip, num_people) {
     let tip_amount = (bill * (1 + tip)) - bill;
 
@@ -32,6 +37,7 @@ function calcResults(bill, tip, num_people) {
     return {"tip_pp": tip_amount_pp, "total_pp": total_pp};
 }
 
+// check if all requirements for calculating results are entered
 function validateForm(){
     if ((chosen_tip != null) && (formBill.value > 0) && (formPeople.value > 0)){
         return true
@@ -40,6 +46,7 @@ function validateForm(){
     }
 }
 
+// Calculates the results and displays them if all requirements are met
 function updateDisplay(){
     if (validateForm()){
         money_data = calcResults(formBill.value, chosen_tip, formPeople.value)
@@ -48,11 +55,30 @@ function updateDisplay(){
     }
 }
 
+// Resets the display values
 function resetDisplay(){
     displayTipPP.textContent = "0.00";
     displayTotalPP.textContent = "0.00";
 }
 
+// Resets the form values
+function restForm(){
+    formBill.value = "";
+    formPeople.value = "";
+    customInput.value = "";
+}
+
+// Clears the error class from all elements and deletes error text
+function resetErrors(){
+    errorTextArray.forEach(element => {
+        element.textContent = "";
+    });
+    errorElementsArray.forEach(element => {
+        element.classList.remove("error")
+    })
+}
+
+// Assigns event handlers for all tip buttons
 tipButtons.forEach(element => {
     element.addEventListener("click", () => {
         if (!element.classList.contains("selected-button")) {
@@ -68,24 +94,29 @@ tipButtons.forEach(element => {
     })
 });
 
-// Need to add regex to limit to decimals
+
+// for some reason when using the arrow buttons to increment the value, it is one click behind
 const customRegex = /^\d*$/
-// Also need to add % after text
 customInput.addEventListener("input", () => {
     if (!(customRegex.test(customInput.value))) { 
         customInput.classList.add("error")
-    } else {
-        customInput.classList.remove("error")
-    }
-    clearSelected()
-    customInput.classList.add("selected-button")
-    chosen_tip = parseFloat(customInput.value / 100)
-    updateDisplay();
-
-    if (customInput.value == "") {
+    } else if (customInput.value == "") {
         customInput.classList.remove("selected-button");
         chosen_tip = null;
+    } else {
+        customInput.classList.remove("error")
+        updateDisplay();
+        clearSelected()
+        customInput.classList.add("selected-button")
+        chosen_tip = parseFloat(customInput.value / 100)
     }
+
+    if (customInput.value < 0) {
+        customInput.value = 0;
+        customInput.classList.remove("error");
+    }
+    
+    
 })
 
 // click handler for when a user enters a value, clicks a different button, and comes back to custom
@@ -97,7 +128,6 @@ customInput.addEventListener("click", () => {
     }  
 })
 
-// Need to change regex to allow format without decimals and assume .00
 const formRegex = /^\d+(\.|\,)\d{2}$/
 const formRegex2 = /^\d*$/
 formBill.addEventListener("input", () => {
@@ -120,18 +150,18 @@ formPeople.addEventListener("input", () => {
     updateDisplay();
     if ((formPeople.value == "") || (formPeople.value <= 0)) { 
         resetDisplay();
-        formPeopleError.textContent = "Can't be 0"
-        formPeople.value = "0"
+        formPeopleError.textContent = "Can't be 0";
+        formPeople.value = "0";
+        formPeople.classList.add("error");
     } else {
-        formPeopleError.textContent = ""
+        formPeopleError.textContent = "";
+        formPeople.classList.remove("error");
     }
 })
 
 resetButton.addEventListener("click", () => {
-    formBill.value = "";
-    formPeople.value = "";
-    customInput.value = "";
+    resetForm();
     resetDisplay();
+    resetErrors();
     clearSelected();
-    // need to reset errors too
 })
